2129329812c55a60321a977e97fa924bd2b31f0d
nangelov1990 <nangelov1990@gmail.com>
2023-12-30 22:22:24 +0200

    Chapter One, C# 1.0

---
+++ CsInDepth/ChapterOne/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+ArrayList list = Product.GetSampleProducts();
+foreach (Product product in list)
+{
+    Console.WriteLine(product);
+}
