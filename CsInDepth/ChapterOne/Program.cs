7621b1da4d2c9d9c53977eed369825dd2789d062
nangelov1990 <nangelov1990@gmail.com>
2023-12-30 23:20:30 +0200

    Separating testing from printing redux, C# 2.0

--- CsInDepth/ChapterOne/Program.cs
+++ CsInDepth/ChapterOne/Program.cs
@@ -2,9 +2,5 @@
 using System.Collections;
 
 List<Product> products = Product.GetSampleProducts();
-
-Predicate<Product> test = delegate(Product p) { return p.Price > 10m; };
-List<Product> matches = products.FindAll(test);
-
-Action<Product> print = Console.WriteLine;
-matches.ForEach(print);
+products.FindAll(delegate(Product p) { return p.Price > 10m; })
+        .ForEach(Console.WriteLine);
