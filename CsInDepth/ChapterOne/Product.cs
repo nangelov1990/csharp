ce221c9803d6fafba6fa331438f82b6e67ebad57
nangelov1990 <nangelov1990@gmail.com>
2023-12-30 22:22:24 +0200

    Chapter One, C# 3.0

--- CsInDepth/ChapterOne/Product.cs
+++ CsInDepth/ChapterOne/Product.cs
@@ -2,39 +2,31 @@ using System.Collections;
 
 public class Product
 {
-    string name;
-    public string Name
-    {
-        get { return name; }
-        private set { name = value; }
-    }
+    public string Name { get; private set; }
+    public decimal Price { get; private set; }
 
-    decimal price;
-    public decimal Price
-    {
-        get { return price; }
-        private set { price = value; }
-    }
-
+    // can be removed completely
     public Product(string name, decimal price)
     {
         Name = name;
         Price = price;
     }
 
+    Product() {}
+
     public static List<Product> GetSampleProducts()
     {
-        List<Product> list = new List<Product>();
-        list.Add(new Product("West Side Story", 9.99m));
-        list.Add(new Product("Assassins", 14.99m));
-        list.Add(new Product("Frogs", 13.99m));
-        list.Add(new Product("Sweenet Todd", 10.99m));
-
-        return list;
+        return new List<Product>
+        {
+            new Product { Name="West Side Story", Price = 9.99m },
+            new Product { Name="Assassins", Price=14.99m },
+            new Product { Name="Frogs", Price=13.99m },
+            new Product { Name="Sweeney Todd", Price=10.99m}
+        };
     }
 
     public override string ToString()
     {
-        return string.Format("{0}: {1}", name, price);
+        return string.Format("{0}: {1}", Name, Price);
     }
 }
